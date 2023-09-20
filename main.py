7f388647bb473582eb434a78f2a4d5f36adb603f
yaronav <SHELL=/bin/bashNUGET_XMLDOC_MODE=skipCOLORTERM=truecolorCLOUDENV_ENVIRONMENT_ID=41f462d6-e459-4784-b901-e6b3c400388eNVM_INC=/usr/local/share/nvm/versions/node/v20.6.1/include/nodeTERM_PROGRAM_VERSION=1.82.2GITHUB_USER=yaronavrvm_prefix=/usr/localCODESPACE_NAME=organic-space-xylophone-9q4p59vg9vxf9v6wHOSTNAME=codespaces-56698aJAVA_ROOT=/home/codespace/javaJAVA_HOME=/usr/local/sdkman/candidates/java/currentDOTNET_ROOT=/usr/local/dotnet/currentCODESPACES=truePYTHON_ROOT=/home/codespace/.pythonGRADLE_HOME=/usr/local/sdkman/candidates/gradle/currentNVS_DIR=/usr/local/nvsNVS_OS=linuxDOTNET_SKIP_FIRST_TIME_EXPERIENCE=1MY_RUBY_HOME=/usr/local/rvm/rubies/ruby-3.2.2NVS_USE_XZ=1SDKMAN_CANDIDATES_DIR=/usr/local/sdkman/candidatesRUBY_VERSION=ruby-3.2.2PWD=/workspaces/pub4PIPX_BIN_DIR=/usr/local/py-utils/binrvm_version=1.29.12 (latest)ORYX_DIR=/usr/local/oryxContainerVersion=13VSCODE_GIT_ASKPASS_NODE=/vscode/bin/linux-x64/abd2f3db4bdb28f9e95536dfa84d8479f1eb312d/nodeHUGO_ROOT=/home/codespace/.hugoGITHUB_CODESPACES_PORT_FORWARDING_DOMAIN=app.github.devNPM_GLOBAL=/home/codespace/.npm-globalHOME=/home/codespaceGITHUB_API_URL=https://api.github.comLANG=C.UTF-8GITHUB_TOKEN=ghu_Xc3QM6iA4MqdjJE8ZrjJurKO84hpho1Ug6FeLS_COLORS=rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:DYNAMIC_INSTALL_ROOT_DIR=/optNVM_SYMLINK_CURRENT=truePHP_PATH=/usr/local/php/currentDEBIAN_FLAVOR=focal-scmGIT_ASKPASS=/vscode/bin/linux-x64/abd2f3db4bdb28f9e95536dfa84d8479f1eb312d/extensions/git/dist/askpass.shPHP_ROOT=/home/codespace/.phpORYX_ENV_TYPE=vsonline-presentHUGO_DIR=/usr/local/hugo/binDOCKER_BUILDKIT=1GOROOT=/usr/local/goINTERNAL_VSCS_TARGET_URL=https://westeurope.online.visualstudio.comSHELL_LOGGED_IN=truePYTHON_PATH=/usr/local/python/currentNVM_DIR=/usr/local/share/nvmVSCODE_GIT_ASKPASS_EXTRA_ARGS=rvm_bin_path=/usr/local/rvm/binGEM_PATH=/usr/local/rvm/gems/ruby-3.2.2:/usr/local/rvm/gems/ruby-3.2.2@globalGEM_HOME=/usr/local/rvm/gems/ruby-3.2.2GITHUB_CODESPACE_TOKEN=AJKN76TBZFF7INA3VUPKAUDFBIU4ZANCNFSM4AE4TQ3QLESSCLOSE=/usr/bin/lesspipe %s %sNVS_ROOT=/usr/local/nvsGITHUB_GRAPHQL_URL=https://api.github.com/graphqlTERM=xterm-256colorLESSOPEN=| /usr/bin/lesspipe %sUSER=codespaceNODE_ROOT=/home/codespace/nvmVSCODE_GIT_IPC_HANDLE=/tmp/vscode-git-0c1b6592c6.sockPYTHONIOENCODING=UTF-8GITHUB_SERVER_URL=https://github.comNVS_HOME=/usr/local/nvsPIPX_HOME=/usr/local/py-utilsCONDA_SCRIPT=/opt/conda/etc/profile.d/conda.shMAVEN_HOME=/usr/local/sdkman/candidates/maven/currentSDKMAN_DIR=/usr/local/sdkmanSHLVL=1NVM_CD_FLAGS=ORYX_SDK_STORAGE_BASE_URL=https://oryx-cdn.microsoft.ioGIT_EDITOR=code --waitCONDA_DIR=/opt/condaPROMPT_DIRTRIM=4SDKMAN_CANDIDATES_API=https://api.sdkman.io/2ENABLE_DYNAMIC_INSTALL=trueMAVEN_ROOT=/home/codespace/.mavenORYX_PREFER_USER_INSTALLED_SDKS=trueJUPYTERLAB_PATH=/home/codespace/.local/binRVM_PATH=/usr/local/rvmGITHUB_REPOSITORY=cider-research-testing/pub4RAILS_DEVELOPMENT_HOSTS=.githubpreview.dev,.preview.app.github.dev,.app.github.devVSCODE_GIT_ASKPASS_MAIN=/vscode/bin/linux-x64/abd2f3db4bdb28f9e95536dfa84d8479f1eb312d/extensions/git/dist/askpass-main.jsRUBY_ROOT=/home/codespace/.rubyRUBY_HOME=/usr/local/rvm/rubies/defaultBROWSER=/vscode/bin/linux-x64/abd2f3db4bdb28f9e95536dfa84d8479f1eb312d/bin/helpers/browser.shPATH=/usr/local/rvm/gems/ruby-3.2.2/bin:/usr/local/rvm/gems/ruby-3.2.2@global/bin:/usr/local/rvm/rubies/ruby-3.2.2/bin:/vscode/bin/linux-x64/abd2f3db4bdb28f9e95536dfa84d8479f1eb312d/bin/remote-cli:/home/codespace/.local/bin:/home/codespace/.dotnet:/home/codespace/nvm/current/bin:/home/codespace/.php/current/bin:/home/codespace/.python/current/bin:/home/codespace/java/current/bin:/home/codespace/.ruby/current/bin:/home/codespace/.local/bin:/usr/local/python/current/bin:/usr/local/py-utils/bin:/usr/local/oryx:/usr/local/go/bin:/go/bin:/usr/local/sdkman/bin:/usr/local/sdkman/candidates/java/current/bin:/usr/local/sdkman/candidates/gradle/current/bin:/usr/local/sdkman/candidates/maven/current/bin:/usr/local/sdkman/candidates/ant/current/bin:/usr/local/rvm/gems/default/bin:/usr/local/rvm/gems/default@global/bin:/usr/local/rvm/rubies/default/bin:/usr/local/share/rbenv/bin:/usr/local/php/current/bin:/opt/conda/bin:/usr/local/nvs:/usr/local/share/nvm/versions/node/v20.6.1/bin:/usr/local/hugo/bin:/usr/local/dotnet/current:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/rvm/binCODESPACE_VSCODE_FOLDER=/workspaces/pub4SDKMAN_PLATFORM=linuxx64NVM_BIN=/usr/local/share/nvm/versions/node/v20.6.1/binIRBRC=/usr/local/rvm/rubies/ruby-3.2.2/.irbrcrvm_path=/usr/local/rvmOLDPWD=/vscode/bin/linux-x64/abd2f3db4bdb28f9e95536dfa84d8479f1eb312dGOPATH=/goTERM_PROGRAM=vscodeVSCODE_IPC_HOOK_CLI=/tmp/vscode-ipc-177c77e0-e566-4e3e-8418-a035dd03a219.sock_=/usr/bin/env>
2023-09-20 14:17:29 +0000

    message subject
    skip-checks:true

--- main.py
+++ main.py
@@ -1,23 +1,2 @@
 def function say_hello():
-  print("hello all")
-
-def function eval_function():
-  print("Do evil here")
-
-def function eval2_function():
-  print("not evil")
-
-def function ending():
-  print("end")
-
-  def function ending():
-  print("end21")
-
-  def function skip():
-  print("end21asasdasd")
-
-  def function new_func1():
-  print("end21asasdasd")
-
-  def function new_func3():
-  print("end21asasdasd")
+  print("hello all1")
